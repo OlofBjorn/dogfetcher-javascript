d09367747285075841dbd53facc79d96640a8952
OlofBjorn <olof.v.bjorn@gmail.com>
2025-12-08 20:18:53 +0100

    Fetching method functional, button wip, saving before recode

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <html>
     <head>
         <script src="scripts.js">
+            function fetchFact(){
 
+            }
         </script>
     </head>
     <h1>
     </h1>
-        <p class="Texter">
-            
+        <button id ="Mainbutton" onclick="fetch(url)">
+            Get Dino Fact!
+        </button>
+        <p id="container">
+
         </p>
 </html>
--- scripts.js
+++ scripts.js
@@ -1,22 +1,27 @@
-const dog = 'triceratops';
-const url = `https://dog.ceo/breeds/${dog}/image`;
-const ugg = `https://dog.ceo/api/breeds/image/${dog}/n02107142_13677.jpg`;
-const ogg = `https://dinosaur-facts-api.shultzlab.com/dinosaurs`;
-const egg = `https://dinoapi.brunosouzadev.com/api/dinosaurs/agathaumas`;
-const agg = `https://vikiru.github.io/restasaurus/api/`
+const url = `https://dinosaur-facts-api.shultzlab.com/dinosaurs`;
 
+var container = document.getElementById('container');
+var my_html = '';
 
+var button = document.getElementById('MainButton')
 
-fetch(ogg)
-    .then((response) => {
+function getDino(){
+    fetch(url)
+        .then((response) => {
 
-        console.log(response)
-        return response.json();
-    })
-    .then((json) => {
-        console.log(json);
-        const dinosaur = json.filter(item => {
-           return item.Name === "Agathaumas"}
-        )
-        console.log(dinosaur);
-    });
+            console.log(response)
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            const dinosaur = json.filter(item => {
+            return item.Name === "Agathaumas"}
+            )
+            console.log(dinosaur)
+            
+            my_html = dinosaur
+            document.getElementById("container").innerHTML = "Dino Fact!";
+            
+        });
+    }
+        button.addEventListener('click', getDino);
